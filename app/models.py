c7ee52f13e0e3f7835a03545a35396f17946e028
Md Al Amin <mdalaminbijoy3@gmail.com>
2024-01-09 01:21:04 +0600

    added category, ProductPhoto models and made some changes in Product model

--- app/models.py
+++ app/models.py
@@ -6,12 +6,27 @@ from datetime import datetime
 
 
 # TODO: Category table should be added
+class Category(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField(blank=True, null=True)
+
+	def __str__(self) -> str:
+		return self.name
+	
+
+PRODUCT_CONDITION_CHOICES = [
+	('new', 'New'),
+	('open_box', 'Open box'),
+	('used', 'Used'),
+]
+
 
 class Product(models.Model):
-	# TODO: One-to-One link to the Category items
 	name = models.CharField(max_length=200, verbose_name='Product Name',)
+	category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='products')
+	condition = models.CharField(max_length=30, choices=PRODUCT_CONDITION_CHOICES, verbose_name='Product Condition')
 	description = models.TextField(null=True, blank=True)
-	photo = models.ImageField(null=True, blank=True, upload_to='uploads/%Y-%m-%d', default='default.png')
+	# photo = models.ImageField(null=True, blank=True, upload_to='uploads/%Y-%m-%d', default='default.png')
 	min_bid_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Minimum Bidding Price')
 	auc_end_time = models.DateTimeField(verbose_name='Bid Ends at ')
 	created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -34,3 +49,8 @@ class Auction(models.Model):
 	placed_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	amount = models.DecimalField(max_digits=10, decimal_places=2)
 	placed_datetime = models.DateTimeField(auto_now_add=True)
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, default=None, null=True, blank=True, related_name='product_images')
+    image = models.ImageField(upload_to='uploads')
