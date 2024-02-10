1afe423ccc896672eb616f55b93f5f67bdc55cee
Md Al Amin <mdalaminbijoy3@gmail.com>
2024-02-10 16:32:07 +0600

    get debug mode on/off from .env

--- abay/settings.py
+++ abay/settings.py
@@ -17,8 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG_MODE", False) == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', '192.168.1.116', 'spike87.pythonanywhere.com', '34.28.229.204']
 
